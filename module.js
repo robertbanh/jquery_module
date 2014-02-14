69651ad13a8ab3c60e8e2322d1fcf508ca946842
Robert Banh <robert.banh@gmail.com>
2014-02-14 13:48:24 -0600

    adding new module.js

---
+++ module.js
@@ -0,0 +1,49 @@
+/*!
+ * module.js
+ *
+ * Sample module file
+ *
+ */
+(function(window, $, PubSub) {
+    "use strict";
+
+    /*
+     * variables
+     *
+     */
+    var _x = localStorage.x;
+
+    /*
+     * Private methods
+     *
+     */
+    function _getStuff()
+    {
+        console.log('getting stuff');
+    };
+
+    /*
+     * Public methods
+     *
+     */
+    var methods = {
+
+        /*
+         * fetch stuff function
+         *
+         */
+        fetchStuff : function() {
+
+            if (_x)
+                return true;
+
+            return false;
+        }
+
+    }
+
+    // namespace
+    window.CustomModule = methods;
+
+})(window, jQuery, PubSub);
+
